90fab513d0cb30056dd0516cf9a7a969f5324ed6
Hamza Taher <hamzataher535@gmail.com>
2025-12-27 21:45:58 +0200

    new feauters booking

--- models/seat.py
+++ models/seat.py
@@ -1,8 +1,9 @@
 class Seat:
-    def __init__(self, seat_id, plane_id, seat_class):
+    def __init__(self, seat_id, plane_id, seat_class, is_available=True):
         self.seat_id = seat_id
         self.plane_id = plane_id
         self.seat_class = seat_class  # A, B, or C
+        self.is_available = is_available
 
     def __repr__(self):
-        return f"<Seat {self.seat_id} | Plane {self.plane_id} | Class {self.seat_class}>"
+        return f"<Seat {self.seat_id} | Plane {self.plane_id} | Class {self.seat_class} | Available {self.is_available}>"
